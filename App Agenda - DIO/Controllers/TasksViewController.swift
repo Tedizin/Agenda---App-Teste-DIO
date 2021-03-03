faaea63e5c5794b79f692b659c276b19138528ae
Henrique Silva <henriquesilva@Mac-mini-de-Henrique.local>
2021-03-03 18:12:48 -0300

    Primeiro Commit

---
+++ App Agenda - DIO/Controllers/TasksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TasksViewController.swift
+//  App Agenda - DIO
+//
+//  Created by Henrique Silva on 03/03/21.
+//
+
+import UIKit
+
+class TasksViewController: UISearchController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
